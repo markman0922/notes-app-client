f04c712f32280ad268dac55c4692108dbb8563e6
Mark Mandell <markman0922@gmail.com>
2020-05-08 14:12:19 -0700

    sets up react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-west-2",
+        BUCKET: "mark-notes-app-uploads"
+    },
+    apiGateway: {
+        REGION: "us-west-2",
+        URL: "https://3bkxgpxkrh.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_OVKY0MCTT",
+        APP_CLIENT_ID: "5ga572hdcdiq5pgb1u676p4qnp",
+        IDENTITY_POOL_ID: "us-west-2:29f35331-1336-42af-99d0-84f71c057cba"
+    }
+};
