51a807cc58fcd8fc69548d3ce181542b9eba8db7
Mark Mandell <markman0922@gmail.com>
2020-05-09 12:48:16 -0700

    v1 notes app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
         REGION: "us-west-2",
         BUCKET: "mark-notes-app-uploads"
@@ -12,5 +13,6 @@ export default {
         USER_POOL_ID: "us-west-2_OVKY0MCTT",
         APP_CLIENT_ID: "5ga572hdcdiq5pgb1u676p4qnp",
         IDENTITY_POOL_ID: "us-west-2:29f35331-1336-42af-99d0-84f71c057cba"
-    }
+    },
+    STRIPE_KEY: "pk_test_iaEUsUGjGiGJyqGGGzFC9f6N",
 };
